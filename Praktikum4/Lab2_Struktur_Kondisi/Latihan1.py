6e23b5f7bdaf8f5eeb245ac871e4ab501212aef4
Fadzarsw <Fadzar19@gmail.com>
2023-11-06 22:15:31 +0700

    first commit

---
+++ Praktikum4/Lab2_Struktur_Kondisi/Latihan1.py
@@ -0,0 +1,11 @@
+num = int(input("masukkan bilangan: "))
+num2 = int(input("masukkan bilangan: "))
+
+# Nilai Terbesar
+if num > num2:
+  print("bilangan terbesar: ",num)
+
+# Nilai Terkecil
+else:
+  print("bilangan terbesar: ",num2)
+
